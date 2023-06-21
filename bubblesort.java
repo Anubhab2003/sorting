ab42e35c92ec4674a4db6b6943fd7f88e96689a7
Anubhab Chowdhury <99398916+Anubhab2003@users.noreply.github.com>
2023-06-21 15:57:07 +0000

    Example of Bubble, selection and insertion sort

--- bubblesort.java
+++ bubblesort.java
@@ -28,6 +28,18 @@ public class bubblesort {
             arr[i] = temp;
         }
     }
+    public static void insertionsort(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            //Insertion
+            arr[prev+1]=curr;
+        }
+    }
 
     public static void printarr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
@@ -38,7 +50,7 @@ public class bubblesort {
 
     public static void main(String args[]) {
         int arr[] = { 5, 4, 1, 3, 2 };
-        selectionsort(arr);
+        insertionsort(arr);
         printarr(arr);
     }
 }
